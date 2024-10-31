431fcb6ab60256b7dea7a38834b5041765dea694
Edwin <correo@mail.com>
2024-10-31 18:03:24 -0400

    archivos modificados

---
+++ src/app/inicio/inicio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-inicio',
+    templateUrl: './inicio.component.html',
+    styleUrls: ['./inicio.component.css']
+    // template: `<h1>Esta es la página de inicio</h1>`  // Equivalente al selector anterior.  // Este template se puede omitir si se utiliza templateUrl.  // El styleUrls se utiliza para aplicar estilos a las etiquetas HTML que se encuentren en el template.  // El styleUrls puede ser un array de URLs de archivos CSS o un array de cadenas de estilos CSS.  // Los estilos se aplican en orden de la lista en el array.  // Las etiquetas HTML se encuentran en el template se pueden utilizar para mostrar información o para realizar interacciones con el usuario.  // Las etiquetas HTML se pueden utilizar para mostrar información o para realizar interacciones con el usuario.  // Las etiquetas
+})
+export class InicioComponent{
+
+}
